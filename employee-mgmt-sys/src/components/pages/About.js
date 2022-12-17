f01a2cf3345e03acdb347d79735345af37ac9133
k-shivam <42085548+k-shivam@users.noreply.github.com>
2022-12-17 20:29:18 +0530

    Create About.js

---
+++ employee-mgmt-sys/src/components/pages/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = props => {
+  return (
+    <div className='container'>
+        <div className='py-4'>
+        <h3>About View</h3>
+        <p className='lead'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+        dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        </div>
+    </div>
+  );
+};
+
+export default About;
