ac1fd461916e3987ea39c856d396938b484c4735
k-shivam <42085548+k-shivam@users.noreply.github.com>
2022-12-19 19:27:54 +0530

    Update App.js

--- employee-mgmt-sys/src/App.js
+++ employee-mgmt-sys/src/App.js
@@ -8,6 +8,8 @@ import PageNotFound from "./components/pages/PageNotFound";
 import AddUser from "./components/users/AddUser";
 import EditUser from "./components/users/EditUser";
 import User from "./components/users/User";
+import Login from "./components/users/Login";
+import Register from "./components/users/Register";
 
 function App() {
   return (
@@ -17,13 +19,29 @@ function App() {
         <Navbar/>
         </div>
         <Routes>
-          <Route exact path="/" element={<h1>Home Page</h1>}/>
+          <Route exact path="/" element={<div className='container'>
+        <div className='py-4'>
+        <h3>Home Page</h3>
+        <p className='lead'>
+        Hi, This is simple website to add users and see list of users after you are signed in.
+        <br/>
+        <br/>
+        After going to list page you can see the detail of a particular user. Take actions like edit, or can delete
+        the user from the list.
+        <br/>
+        <br/>
+        But before that you have to create an account, and if account is already there you need to sign in.
+        </p>
+        </div>
+    </div>}/>
           <Route exact path="/home" element={<Home/>}/>
           <Route exact path="/about" element={<About/>}/>
           <Route exact path="/contact" element={<Contact/>}/>
           <Route exact path="/user/add" element={<AddUser/>}/>
           <Route exact path="/user/edit/:id" element={<EditUser/>}/>
           <Route exact path="/user/view/:id" element={<User/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/register" element={<Register/>}/>
           <Route exact path="*" element={<PageNotFound/>}/>
         </Routes>
       </Router>
